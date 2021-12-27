052edc5fccf7fa17f94ffdd58f161065d59a25e8
ginogi21 <92210791+ginogi21@users.noreply.github.com>
2021-12-27 14:47:43 +0900

    file add

--- Web/JavaScript/ex25/ex25.js
+++ Web/JavaScript/ex25/ex25.js
@@ -1,10 +1,15 @@
-const person = {
-  name:'Lee',
-  foo(callback){
-    setTimeout(callback.bind(this), 100);
-  }
-};
+let bg = document.getElementById('bg');
+let moon = document.getElementById('moon');
+let mountain = document.getElementById('mountain');
+let road = document.getElementById('road');
+let text = document.getElementById('text');
 
-person.foo(function(){
-  console.log(`Hi! My name is ${this.name}`);
-});
+window.addEventListener('scroll',function(){
+  var value = window.scrollY;
+
+  bg.style.top = value *0.5 + 'px';
+  moon.style.left = -value * 0.5 + 'px';
+  mountain.style.top = -value * 0.15 + 'px';
+  road.style.top = value * 0.15 + 'px';
+  text.style.top = value * 1 + 'px';
+})
